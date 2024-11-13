9a31f2b544731beaaed6f959ccca3d14514894da
Osereme-Onyoreme Ibazebo <osibazeb@scs.ryerson.ca>
2024-11-13 09:25:42 -0500

    perl stuff working

--- lab07/name.pl
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/perl -wT
-use CGI':standard';
-use CGI::Carp qw(warningsToBrowser fatalsToBrowser);
-print "Content-type: text/html\n\n";
-my $name = param ('name');
-print "Hello $name";
