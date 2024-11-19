865277a455d026fd185e906ebcd063c973673d28
osereme <reme.ibazebo@gmail.com>
2024-11-19 16:27:54 -0500

    push

--- lab08/index.php
+++ lab08/index.php
@@ -4,36 +4,38 @@
         <title>
             LAB 08
         </title>
+        <link rel="stylesheet" href="./styles.css">
     </head> 
     <body>
         <div>
-            <H1>
-                TASK 1
-            </H1>
+            <H1>TASK 1</H1>
            <div>
-            <?php 
-                $hour= date("H");
+<?php 
+    $hour= date("H");
 
-                if ($hour >= 21 || $hour < 5){
-                    $message = "<h1>GOOD NIGHT</h1>";
-                }
-                elseif ($hour >= 18){
-                    $message = "<h1>GOOD EVENING</h1>";
-                }
-                elseif ($hour >= 12){
-                    $message = "<h1>GOOD AFTERNOON</h1>";
-                }
-                else{
-                    $message = "<h1>GOOD MORNING</h1>";
-                }
+    if ($hour >= 21 || $hour < 5){
+        $message = "<h1>GOOD NIGHT</h1>";
+        $image = "night"
+    }
+    elseif ($hour >= 18){
+        $message = "<h1>GOOD EVENING</h1>";
+        $image = "evening"
+    }
+    elseif ($hour >= 12){
+        $message = "<h1>GOOD AFTERNOON</h1>";
+        $image = "afternoon"
+    }
+    else{
+        $message = "<h1>GOOD MORNING</h1>";
+        $image = "morning"
+    }
 echo <<<T1
-<div>
-THE MESSAGE IS
+<div class="$image">
 <br>
 $message
 </div>
 T1;
-            ?>
+?>
            </div> 
         </div>
     </body>
