2c1520781854850adeced9b3f3fdc532a1e8a44b
Osereme-Onyoreme Ibazebo <osibazeb@scs.ryerson.ca>
2024-11-19 15:47:11 -0500

    ssh changes

--- lab08/index.php
+++ lab08/index.php
@@ -14,16 +14,16 @@
             <?php 
                 $hour= date("H");
                 if ($hour > 21 || $hour < 5){
-                    echo "<h1>Good Night</h1>";
+                    echo "<h1>GOOD NIGHT</h1>";
                 }
                 elseif ($hour > 17){
-                    echo "<h1>Good Evening</h1>";
+                    echo "<h1>GOOD EVENING</h1>";
                 }
                 elseif ($hour > 11){
-                    echo "<h1>Good Afternoon</h1>";
+                    echo "<h1>GOOD AFTERNOON</h1>";
                 }
                 elseif ($hour > 4){
-                    echo "<h1>Good Morning</h1>";
+                    echo "<h1>GOOD MORNING</h1>";
                 }
 
             ?>
