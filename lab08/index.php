3353d12821ff71824ec70f3fd765234d4da49f84
osereme <reme.ibazebo@gmail.com>
2024-11-20 09:22:01 -0500

    add in forms

--- lab08/index.php
+++ lab08/index.php
@@ -33,7 +33,31 @@
 </div>
 T1;
 
+if ($_SERVER["REQUEST_METHOD"] == "PSOT"){
+    $selectedGif = $_POST['gif'] ?? 'None';
+    echo "<div><h2>You selected: $selectedGif</h2></div>";
+}
+
 ?>
+    <div>
+        <form>
+            <h3>
+                Select a GIF
+            </h3>
+            <input type="radio" name="gif" id="corn">
+            <label for="corn">Thanksgiving Corn</label><br>
+            <input type="radio" name="gif" id="pie">
+            <label for="pie">Thanksgiving Pie</label><br>
+            <input type="radio" name="gif" id="wreath">
+            <label for="wreath">Thanksgiving Wreath</label><br>
+            <input type="radio" name="gif" id="turkey1">
+            <label for="turkey1">Thanksgiving Turkey 1</label><br>
+            <input type="radio" name="gif" id="turkey2">
+            <label for="turkey2">Thanksgiving Turkey 2</label><br>
+            <br>
+            <input type="submit">
+        </form>
+    </div>
            </div> 
         </div>
     </body>
