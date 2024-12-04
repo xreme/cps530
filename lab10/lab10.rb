9a5785dd7720b6935d236ea9fccb87f741070825
osereme <reme.ibazebo@gmail.com>
2024-12-03 19:48:51 -0500

    ruby work

--- lab10/lab10.rb
+++ lab10/lab10.rb
@@ -3,7 +3,21 @@ require 'cgi'
 cgi = CGI.new
 print "Content-type: text/html\n\n"
 
-puts cgi['city']
-puts cgi['province']
-puts cgi['country']
-puts cgi['image_url']
+city = cgi['city']
+province = cgi['province']
+country = cgi['country']
+image_url = cgi['image_url']
+
+result = ''
+if !city.empty? && !country.empty?
+    result = result + city
+    if !province.empty?
+        result = result + ", " + empty
+    end
+    result = result + ", " + country
+else
+    puts "<h1> No City and/or Country Given</h1>"
+end
+puts "<h1 style=\"background-color: rgb(252, 48, 191); \">"
+puts result
+puts "</h1>"
